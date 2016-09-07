5d0ca404e8de7b486d10da634ca86c3ba5c95e08
alexberry721 <alexberry721@gmail.com>
2016-09-07 09:30:00 -0700

    Init commit

--- Book Exercise 3.24 AND OR Operators/main.cpp
+++ Book Exercise 3.24 AND OR Operators/main.cpp
@@ -7,9 +7,52 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+
+    cout << "Testing AND OR Operators with Integer Division"  << endl;
+    
+    // Data
+    int integer_input = 0;
+    bool div_by_5 = 0;
+    bool div_by_6 = 0;
+    
+    // Input
+    cout << "Enter an integer: ";
+    cin >> integer_input;
+    
+    // Processing
+    
+    // modulus test for divisibility
+    div_by_5 = integer_input % 5 == 0 ? 1 : 0;
+    div_by_6 = integer_input % 6 == 0 ? 1 : 0;
+    
+    // Output
+    if (div_by_5 && div_by_6) {
+        cout << "TTF"  << endl;
+        cout << "Is " << integer_input << " divisible by 5 and 6? True" << endl;
+        cout << "Is " << integer_input << " divisible by 5 or 6? True" << endl;
+        cout << "Is " << integer_input << " divisible by 5 or 6, but not both? False" << endl;
+        
+    } else if (div_by_5 || div_by_6) {
+        cout << "FTT"  << endl;
+        cout << "Is " << integer_input << " divisible by 5 and 6? False" << endl;
+        cout << "Is " << integer_input << " divisible by 5 or 6? True" << endl;
+        cout << "Is " << integer_input << " divisible by 5 or 6, but not both? True" << endl;
+        
+    } else if ((div_by_5 || div_by_6) && !(div_by_5 && div_by_6)) {
+        cout << "FTT"  << endl;
+        cout << "Is " << integer_input << " divisible by 5 and 6? False" << endl;
+        cout << "Is " << integer_input << " divisible by 5 or 6? True" << endl;
+        cout << "Is " << integer_input << " divisible by 5 or 6, but not both? True" << endl;
+    } else {
+        cout << "Err: 0 Case"  << endl;
+        cout << "Is " << integer_input << " divisible by 5 and 6? False" << endl;
+        cout << "Is " << integer_input << " divisible by 5 or 6? False" << endl;
+        cout << "Is " << integer_input << " divisible by 5 or 6, but not both? False" << endl;
+    }
+    
     return 0;
 }
+
